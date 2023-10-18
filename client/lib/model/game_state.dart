f211e81256f192767ce12342fadbe908c6da421d
collinshayden <collinshayden171@gmail.com>
2023-10-18 15:20:29 -0400

    wip

--- client/lib/model/game_state.dart
+++ client/lib/model/game_state.dart
@@ -2,7 +2,10 @@
 /// Author: Hayden Collins
 /// Date: 10/15/2023
 
+import "package:flutter/material.dart";
+
 import "../model/utils/endpoint.dart";
+
 
 // just defining as a string because I was having difficulty reading from json file
 const jsonData = '''
@@ -36,10 +39,38 @@ const jsonData = '''
   }
 ''';
 
+class PlayerData {
+  PlayerData({required this.playerID, required this.displayName, this.money, this.location, this.properties, this.jail_card});
+  int playerID;
+  String displayName;
+  int? money;
+  int? location;
+  Map<String, dynamic>? properties;
+  int? jail_card;
+
+} // end PlayerData
+
+class PlayerManager {
+  PlayerManager({required this.players});
+  Map<int, PlayerData> players = {};
+
+  void addPlayer(Map<String, dynamic> data) {
+    players[data["player_id"]] = PlayerData(playerID: data["player_id"], displayName: data["displayName"]);
+  }
+  // TODO implement update method
+  void setPlayerData(Map<String, dynamic> data) {
+    PlayerData player = players[data["player_id"]]!; 
+    player
+    ..displayName = data["displayName"]
+    ..money = data["money"]
+    ..location = data["location"]
+    ..jail_card = data["jail_card"];
+    
+  }
+} // end PlayerManager
 
 
 main() {
   final playerInfo = readJson(jsonData);
-  PlayerDataManager manager = PlayerDataManager.fromJson(playerInfo);
-
+  PlayerManager manager = PlayerManager.fromJson(playerInfo);
 }
