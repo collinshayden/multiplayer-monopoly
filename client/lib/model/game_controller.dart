f268f7553b5815368b3d8f446c045ca1e7f1451c
agkhall <contact@agkhall.info>
2023-10-22 21:11:53 -0400

    Cleaned up old imports.

--- client/lib/model/game_controller.dart
+++ client/lib/model/game_controller.dart
@@ -2,19 +2,13 @@
 /// Author: Hayden Collins
 /// Date: 10/15/2023
 
-import "package:flutter/material.dart";
-
 import "endpoint_manager.dart";
-
-import "package:json_annotation/json_annotation.dart";
 
 class GameController {
   GameController();
   final _endpoint = EndpointManager();
 
   void rollDice() {
-    //
-
-    //update the game state
+    // TODO: implement connection with server an
   }
 }
