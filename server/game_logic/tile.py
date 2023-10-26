0d7a8f99495438e1362c5a39cce5fcb85927c206
A-Bonner <aidanbonner140202@gmail.com>
2023-10-26 16:02:16 -0400

    added id field to tile superclass

--- server/game_logic/tile.py
+++ server/game_logic/tile.py
@@ -7,9 +7,10 @@ Author:         Jordan Bourdeau, Hayden Collins
 
 class Tile:
 
-    def __init__(self) -> None:
+    def __init__(self, id: int) -> None:
         """
         Description:    Class representing a Tile on the board.
+        :param id:      An integer identifier for each tile.
         :returns:       None.
         """
         
