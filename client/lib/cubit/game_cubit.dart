21ec58d7f5a22697647a34561bd728b5b4131463
agkhall <contact@agkhall.info>
2023-10-31 18:07:09 -0400

    Added endpoint service and emit calls

--- client/lib/cubit/game_cubit.dart
+++ client/lib/cubit/game_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:client/cubit/file_service.dart';
+import 'package:client/cubit/endpoint_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'game_state.dart';
 
@@ -6,9 +7,12 @@ class GameCubit extends Cubit<GameState> {
   GameCubit() : super(GameInitial());
 
   final fileService = FileService();
+  final endpointService = EndpointService();
 
   void loadLocalConfig() {
+    emit(LocalConfigLoading());
     final boardConfig = fileService.readFile('board_config.json');
     print(boardConfig);
+    emit(LocalConfigSuccess());
   }
 }
