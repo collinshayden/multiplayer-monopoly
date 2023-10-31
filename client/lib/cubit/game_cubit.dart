1fc668ecee6c7716439e825e0d1748b1352f7653
agkhall <contact@agkhall.info>
2023-10-31 17:21:53 -0400

    Added FileService to Cubit

--- client/lib/cubit/game_cubit.dart
+++ client/lib/cubit/game_cubit.dart
@@ -1,7 +1,14 @@
+import 'package:client/controller/file_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'game_state.dart';
 
 class GameCubit extends Cubit<GameState> {
   GameCubit() : super(GameInitial());
-}
 
+  final fileService = FileService();
+
+  void loadLocalConfig() {
+    final boardConfig = fileService.readFile('board_config.json');
+    print(boardConfig);
+  }
+}
