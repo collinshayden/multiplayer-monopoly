6009b26ea47996083a998496945f601f11314ff2
collinshayden <collinshayden171@gmail.com>
2023-11-02 17:14:21 -0400

    created TileData subclasses

--- client/lib/model/tile_data.dart
+++ client/lib/model/tile_data.dart
@@ -1,17 +1,16 @@
+import "dart:ui";
+
+import "package:client/view/base/tiles.dart";
 import "package:json_annotation/json_annotation.dart";
 
 part 'tile_data.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class TileData {
-  int id;
-  int owner;
-  int improvements;
+  final int id;
 
-  TileData({
+  const TileData({
     required this.id,
-    required this.owner,
-    required this.improvements,
   });
 
   // auto generated factory constructor
@@ -22,7 +21,137 @@ class TileData {
   Map<String, dynamic> toJson() => _$TileDataToJson(this);
 
   void applyJson(Map<String, dynamic> json) {
+    
+  }
+    // empty method
+
+}
+
+@JsonSerializable(explicitToJson: true)
+class ImprovableTileData extends TileData {
+  int? owner;
+  int improvements = 0;
+  String title;
+
+  ImprovableTileData({
+    required super.id,
+    required this.owner,
+    required this.improvements,
+    required this.title,
+  });
+
+  // auto generated factory constructor
+  factory ImprovableTileData.fromJson(Map<String, dynamic> json) =>
+      _$ImprovableTileDataFromJson(json);
+
+  // auto generated factory constructor
+  Map<String, dynamic> toJson() => _$ImprovableTileDataToJson(this);
+
+  void applyJson(Map<String, dynamic> json) {
     owner = json["owner"] ?? owner;
-    improvements = json["improvements"] ?? improvements; // TODO += or = operator? Depends if json["improvements"] is sending a delta or updated value
+    improvements = json["improvements"] ??
+        improvements; // TODO += or = operator? Depends if json["improvements"] is sending a delta or updated value
+  }
+}
+
+@JsonSerializable(explicitToJson: true)
+class CornerTileData extends TileData {
+  String upperText;
+  String imagePath1;
+  String? imagePath2;
+
+  CornerTileData({
+    required super.id,
+    required this.upperText,
+    required this.imagePath1,
+    this.imagePath2,
+  });
+
+  // auto generated factory constructor
+  factory CornerTileData.fromJson(Map<String, dynamic> json) =>
+      _$CornerTileDataFromJson(json);
+
+  // auto generated factory constructor
+  Map<String, dynamic> toJson() => _$CornerTileDataToJson(this);
+
+  void applyJson(Map<String, dynamic> json) {
+    upperText = json["upperText"] ?? upperText;
+    imagePath1 = json["imagePath1"] ?? imagePath1;
+    imagePath2 = json["imagePath2"] ?? imagePath2;
+  }
+}
+
+@JsonSerializable(explicitToJson: true)
+class CommunityTileData extends TileData {
+  String title;
+  String imagePath;
+
+  CommunityTileData({
+    required super.id,
+    required this.title,
+    required this.imagePath,
+  });
+
+  // auto generated factory constructor
+  factory CommunityTileData.fromJson(Map<String, dynamic> json) =>
+      _$CommunityTileDataFromJson(json);
+
+  // auto generated factory constructor
+  Map<String, dynamic> toJson() => _$CommunityTileDataToJson(this);
+
+  void applyJson(Map<String, dynamic> json) {
+    title = json["title"] ?? title;
+    imagePath = json["imagePath"] ?? imagePath;
+  }
+}
+
+@JsonSerializable(explicitToJson: true)
+class ChanceTileData extends TileData {
+  String title;
+  String imagePath;
+
+  ChanceTileData({
+    required super.id,
+    required this.title,
+    required this.imagePath,
+  });
+
+  // auto generated factory constructor
+  factory ChanceTileData.fromJson(Map<String, dynamic> json) =>
+      _$ChanceTileDataFromJson(json);
+
+  // auto generated factory constructor
+  Map<String, dynamic> toJson() => _$ChanceTileDataToJson(this);
+
+  void applyJson(Map<String, dynamic> json) {
+    title = json["title"] ?? title;
+    imagePath = json["imagePath"] ?? imagePath;
+  }
+}
+
+@JsonSerializable(explicitToJson: true)
+class TaxTileData extends TileData {
+  String title;
+  String imagePath;
+  String payCommandText;
+
+  TaxTileData({
+    required super.id,
+    required this.title,
+    required this.imagePath,
+    required this.payCommandText,
+  });
+
+  // auto generated factory constructor
+  factory TaxTileData.fromJson(Map<String, dynamic> json) =>
+      _$TaxTileDataFromJson(json);
+
+  // auto generated factory constructor
+  Map<String, dynamic> toJson() => _$TaxTileDataToJson(this);
+
+  void applyJson(Map<String, dynamic> json) {
+    title = json["title"] ?? title;
+    imagePath = json["imagePath"] ?? imagePath;
+    payCommandText = json["payCommandText"] ?? payCommandText;
   }
 }
--- client/lib/model/tile_data.g.dart
+++ client/lib/model/tile_data.g.dart
@@ -8,12 +8,83 @@ part of 'tile_data.dart';
 
 TileData _$TileDataFromJson(Map<String, dynamic> json) => TileData(
       id: json['id'] as int,
-      owner: json['owner'] as int,
-      improvements: json['improvements'] as int,
     );
 
 Map<String, dynamic> _$TileDataToJson(TileData instance) => <String, dynamic>{
       'id': instance.id,
+    };
+
+ImprovableTileData _$ImprovableTileDataFromJson(Map<String, dynamic> json) =>
+    ImprovableTileData(
+      id: json['id'] as int,
+      owner: json['owner'] as int?,
+      improvements: json['improvements'] as int,
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$ImprovableTileDataToJson(ImprovableTileData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
       'owner': instance.owner,
       'improvements': instance.improvements,
+      'title': instance.title,
+    };
+
+CornerTileData _$CornerTileDataFromJson(Map<String, dynamic> json) =>
+    CornerTileData(
+      id: json['id'] as int,
+      upperText: json['upperText'] as String,
+      imagePath1: json['imagePath1'] as String,
+      imagePath2: json['imagePath2'] as String?,
+    );
+
+Map<String, dynamic> _$CornerTileDataToJson(CornerTileData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'upperText': instance.upperText,
+      'imagePath1': instance.imagePath1,
+      'imagePath2': instance.imagePath2,
+    };
+
+CommunityTileData _$CommunityTileDataFromJson(Map<String, dynamic> json) =>
+    CommunityTileData(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      imagePath: json['imagePath'] as String,
+    );
+
+Map<String, dynamic> _$CommunityTileDataToJson(CommunityTileData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'imagePath': instance.imagePath,
+    };
+
+ChanceTileData _$ChanceTileDataFromJson(Map<String, dynamic> json) =>
+    ChanceTileData(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      imagePath: json['imagePath'] as String,
+    );
+
+Map<String, dynamic> _$ChanceTileDataToJson(ChanceTileData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'imagePath': instance.imagePath,
+    };
+
+TaxTileData _$TaxTileDataFromJson(Map<String, dynamic> json) => TaxTileData(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      imagePath: json['imagePath'] as String,
+      payCommandText: json['payCommandText'] as String,
+    );
+
+Map<String, dynamic> _$TaxTileDataToJson(TaxTileData instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'imagePath': instance.imagePath,
+      'payCommandText': instance.payCommandText,
     };
