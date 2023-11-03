bf072e5adace34045cd5b261018989b108f447cf
collinshayden <collinshayden171@gmail.com>
2023-11-02 21:43:09 -0400

    created parseLocalConfig function

--- client/lib/cubit/game_cubit.dart
+++ client/lib/cubit/game_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:client/cubit/file_service.dart';
 import 'package:client/cubit/endpoint_service.dart';
+import 'package:client/model/tile_data.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'game_state.dart';
 
@@ -22,8 +23,72 @@ class GameCubit extends Cubit<GameState> {
       emit(LocalConfigFailure(e));
     } finally {}
     await Future.delayed(Duration(seconds: 3)); // TODO: Remove
-    emit(LocalConfigSuccess(boardJson: localBoardConfig));
+    
+    emit(LocalConfigSuccess(boardJson: parseLocalConfig(localBoardConfig)));
   }
 
   void loadRemoteConfig() async {}
+}
+
+Map<String, dynamic> parseLocalConfig(Map<String, dynamic> json) {
+  Map<int, TileData> tileData = {};
+  String monetaryUnitName = json["monetaryUnitName"];
+  String monetaryUnitSymbol = json["monetaryUnitSymbol"];
+  List<String> propertyColorsARGB = json["propertyColorsARGB"];
+  for (int i = 0; i < json["tiles"].size(); i++) {
+    Map<String, dynamic> tile = json["tiles"]["${i + 1}"];
+    switch (tile["type"]) {
+      case "corner":
+        tileData[i] = CornerTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            upperText: tile["upperText"],
+            imagePath1: tile["imagePath1"]);
+      case "improvement":
+        tileData[i] = ImprovableTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            owner: tile["owner"],
+            improvements: tile["improvements"],
+            color: tile["color"],
+            price: tile["price"],
+            title: tile["title"]);
+      case "chance":
+        tileData[i] = ChanceTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            title: tile["title"],
+            imagePath: tile["imagePath"]);
+      case "community":
+        tileData[i] = CommunityTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            title: tile["title"],
+            imagePath: tile["imagePath"]);
+      case "railroad":
+        tileData[i] = RailroadTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            title: tile["title"],
+            imagePath: tile["imagePath"],
+            price: tile["price"]);
+      case "utility":
+        tileData[i] = UtilityTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            title: tile["title"],
+            imagePath: tile["imagePath"],
+            price: tile["price"]);
+      case "tax":
+        tileData[i] = TaxTileData(
+            id: tile["id"],
+            quarterTurns: tile["quarterTurns"],
+            title: tile["title"],
+            imagePath: tile["imagePath"],
+            payCommandText: tile["payCommandText"]);
+    }
+  }
+
+  Map<String, dynamic> rtnval = {"tileData" : tileData, "propertyColorsARGB" : propertyColorsARGB};
+  return rtnval;  
 }
