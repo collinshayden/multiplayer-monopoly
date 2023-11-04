b3567fef8f79c3ba3e1a158f60d5ec8fe24d9ec4
agkhall <contact@agkhall.info>
2023-11-04 14:54:06 -0400

    Added roll entry type

---
+++ server/game_logic/roll.py
@@ -0,0 +1,25 @@
+"""
+Description:    Class representing the information for a roll entry.
+Date:           11/03/2023
+Author:         Alex Hall
+"""
+
+from typing import Any
+
+class Roll:
+   def __init__(self, first=None, second=None):
+      self.first: int = first
+      self.second: int = second
+
+   
+   def to_dict(self) -> dict[str, Any]:
+      """
+      Description: Method used as input to the serialiser used in generating network communications made with the client. This function is used as an intermediary representation of the class' data where translations from the frontend (Python) naming conventions to the backend (Flutter/Dart) naming conventions.
+
+      :return: Dictionary from strings to any other data type.
+      """
+      client_bindings: dict[str, Any] = {
+         "first": self.first,
+         "second": self.second
+         }
+      return client_bindings
