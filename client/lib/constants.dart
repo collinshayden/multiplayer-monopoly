e355d891283891c792aeaf7195fbbb5b80569b1d
agkhall <contact@agkhall.info>
2023-11-04 14:43:14 -0400

    Added basic endpoint method

--- client/lib/constants.dart
+++ client/lib/constants.dart
@@ -1,5 +1,3 @@
 const String LOCAL_ASSET_DIR = 'assets'; // Should match `pubspec.yaml` entry.
 
-const String REMOTE_HOST = 'jbourde2.w3.uvm.edu/';
-
-const String REMOTE_API_NAME = 'game';
+const String API_URL = 'http://jbourde2.w3.uvm.edu/game';
--- client/lib/cubit/endpoint_service.dart
+++ client/lib/cubit/endpoint_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:client/model/game_data.dart';
 import 'package:http/http.dart' as http;
 import '../constants.dart';
+import '../json_utils.dart';
 
 class EndpointService {
   /// Internal constructor which should only be called once by the class itself.
@@ -17,9 +18,24 @@ class EndpointService {
   final http.Client server;
 
   Future<GameData> getGameData() async {
-    final response =
-        await server.get(Uri.parse('$REMOTE_HOST/$REMOTE_API_NAME'));
+    final response = await server.get(Uri.parse('$API_URL/state'));
     final json = jsonDecode(response.body);
     return GameData.fromJson(json);
   }
+
+  Future<Json> registerPlayer({required String displayName}) async {
+    assert(!displayName.contains(' ')); // TODO: add more validation
+    final response = await server.get(
+      Uri.parse('$API_URL/register_player?username=$displayName'),
+    );
+    final json = jsonDecode(response.body);
+    return json;
+  }
+}
+
+void main() async {
+  var endpointService = EndpointService();
+  Json registrationResult =
+      await endpointService.registerPlayer(displayName: 'Jason');
+  print(registrationResult);
 }
