1b9fc0b368968e99d3b5a043839471849f81ad8f
agkhall <contact@agkhall.info>
2023-11-13 20:51:24 -0500

    Created skeleton for Event

---
+++ client/lib/model/event.dart
@@ -0,0 +1,21 @@
+enum EventType {
+  startSession,
+  enterQueue,
+  updateQueue,
+  toggleReadyPrompt,
+  startGame,
+  startTurn,
+  showRoll,
+  movePlayer,
+  promptPurchase,
+  showPurchase,
+  showCardDraw,
+  showImprovements,
+  showMortgage,
+  promptEndTurn,
+  showLoser,
+  showWinner,
+  endGame,
+}
+
+class Event {}
