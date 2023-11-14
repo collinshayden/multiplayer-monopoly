a5886425884222166f081aaa71816c2b1e7db54e
collinshayden <collinshayden171@gmail.com>
2023-11-14 14:55:45 -0500

    created rudimentary dice display that updates from lastRoll

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:client/model/player.dart';
 import 'package:client/model/roll.dart';
 import 'package:client/model/tiles.dart';
 import 'package:client/view/base/board.dart';
+import 'package:client/view/dice.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -48,6 +49,7 @@ class _MonopolyAppState extends State<MonopolyApp> {
               // TODO admin buttons will go here
               Spacer(),
               Center(child: AdminButtons()),
+              ShowDice(),
             ],
           ),
         ),
@@ -108,22 +110,18 @@ class AdminButtons extends StatelessWidget {
                 TextButton(
                     onPressed: () {
                       BlocProvider.of<GameCubit>(context).updateGameData();
-                      PlayerId playerId = BlocProvider.of<GameCubit>(context)
-                          .game
-                          .players
-                          .values
-                          .first
-                          .id;
-                      print(playerId);
                       BlocProvider.of<GameCubit>(context)
                           .endpointService
-                          .startGame(playerId: playerId);
+                          .startGame(playerId: PlayerId("admin"));
                     },
                     child: Text('Start Game')),
                 TextButton(
                     onPressed: () {
+                      BlocProvider.of<GameCubit>(context).updateGameData();
                       PlayerId activePlayerId =
-                          BlocProvider.of<GameCubit>(context).game.activePlayerId!;
+                          BlocProvider.of<GameCubit>(context)
+                              .game
+                              .activePlayerId!;
                       BlocProvider.of<GameCubit>(context)
                           .rollDice(playerId: activePlayerId);
                     },
@@ -136,6 +134,29 @@ class AdminButtons extends StatelessWidget {
               ],
             ),
           ),
+        );
+      },
+    );
+  }
+}
+
+class ShowDice extends StatelessWidget {
+  const ShowDice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GameCubit, GameState>(
+      builder: (context, state) {
+        return Container(
+          child: Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: 400,
+                height: 200,
+                child: Row(
+                  children: [Dice(value1: BlocProvider.of<GameCubit>(context).game.lastRoll.first ?? 1, value2: BlocProvider.of<GameCubit>(context).game.lastRoll.second ?? 1)],
+                ),
+              )),
         );
       },
     );
