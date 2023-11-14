3331332b80dba81f9c3b1567c4ff651e04edae1b
agkhall <contact@agkhall.info>
2023-11-14 15:25:24 -0500

    Added event implementation

--- client/lib/model/event.dart
+++ client/lib/model/event.dart
@@ -1,3 +1,5 @@
+import 'package:client/json_utils.dart';
+
 enum EventType {
   startSession,
   enterQueue,
@@ -18,4 +20,14 @@ enum EventType {
   endGame,
 }
 
-class Event {}
+/// Lightweight wrapper class for events passed between the server and client.
+///
+/// This class utilises the [EventType] enumerated type to indicate the type of
+/// event, and parameters specifying what the event is are stored as a [Json]
+/// object `Map<String, dynamic>`.
+class Event {
+  Event({required this.type, required this.parameters});
+
+  EventType type;
+  Json parameters;
+}
