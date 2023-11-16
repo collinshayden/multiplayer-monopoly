178d5af3bff8a02ba468f9e2bd33044d293d2f1b
agkhall <contact@agkhall.info>
2023-11-16 13:58:31 -0500

    Changed get requests and began new polling implementation

--- client/lib/cubit/endpoint_service.dart
+++ client/lib/cubit/endpoint_service.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:async';
+import 'dart:isolate';
 import 'package:http/http.dart' as http;
 import 'package:client/model/player.dart';
 import 'package:client/constants.dart';
@@ -14,69 +16,97 @@ class EndpointService {
 
   /// Instantiate singleton
   static final _instance = EndpointService._internal();
+  static final _timerDuration = Duration(seconds: 1);
 
-  /// Declare a lazily-loaded server connection
+  /// Lazily-loaded object for creating a persistent connection with the server.
   late final http.Client server;
 
-  Future<Json> fetchData() async {
-    final response = await http.get(
-      Uri.parse('$API_URL/data'),
+  /// Declare timer for making calls as an isolate
+  Timer? _timer;
+
+  /// Retrieve a deserialised snapshot of the server's game object.
+  ///
+  /// This function calls the remote API's `state` endpoint
+  void fetchData(Timer t) async {
+    final response = await server.get(
+      Uri.parse('$API_URL/state'),
     );
-    final gameData = jsonDecode(response.body);
-    print(gameData);
-    return gameData;
+    final Json gameData = jsonDecode(response.body);
+    // return gameData;
   }
 
-  void registerPlayer({required String displayName}) async {
-    http.get(
+  Future<bool> registerPlayer({required String displayName}) async {
+    final response = await http.get(
       Uri.parse('$API_URL/register_player?display_name=$displayName'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'registerPlayer');
+    return body['success'];
   }
 
-  void startGame({required PlayerId playerId}) async {
-    http.get(
-      Uri.parse('$API_URL/start_game?player_id=${playerId.value}'),
+  Future<bool> startGame({required PlayerId playerId}) async {
+    final response = await http.get(
+      Uri.parse('$API_URL/start_game?player_id=$playerId'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'startGame');
+    return body['success'];
   }
 
-  void rollDice(PlayerId playerId) async {
-    http.get(
-      Uri.parse('$API_URL/roll_dice?player_id=${playerId.value}'),
+  Future<bool> rollDice(PlayerId playerId) async {
+    final response = await http.get(
+      Uri.parse('$API_URL/roll_dice?player_id=$playerId'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'rollDice');
+    return body['success'];
   }
 
-  void drawCard(String playerId, String cardType) async {
+  Future<bool> drawCard(String playerId, String cardType) async {
     assert(cardType == 'chance' || cardType == 'community_chest');
-    http.get(
+    final response = await http.get(
       Uri.parse('$API_URL/draw_card?player_id=$playerId?card_type=$cardType'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'drawCard');
+    return body['success'];
   }
 
-  void buyProperty(String playerId, int tileId) async {
+  Future<bool> buyProperty(String playerId, int tileId) async {
     assert(0 <= tileId && tileId <= 39);
-    http.get(
+    final response = await http.get(
       Uri.parse('$API_URL/buy_property?player_id=$playerId?tile_id=$tileId'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'buyProperty');
+    return body['success'];
   }
 
-  void setImprovements(String playerId, int tileId, int quantity) async {
+  Future<bool> setImprovements(
+      String playerId, int tileId, int quantity) async {
     assert(0 <= tileId && tileId <= 39);
-    http.get(
+    final response = await http.get(
       Uri.parse(
           '$API_URL/set_improvements?player_id=$playerId?tile_id=$tileId?quantity=$quantity'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'setImprovement');
+    return body['success'];
   }
 
   /// Set the server-side
-  void setMortgage(String playerId, int tileId, bool mortgage) async {
-    http.get(
+  Future<bool> setMortgage(String playerId, int tileId, bool mortgage) async {
+    final response = await http.get(
       Uri.parse(
           '$API_URL/set_mortgage?player_id=$playerId?tile_id=$tileId?mortgage=$mortgage'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'setMortgage');
+    return body['success'];
   }
 
-  void chooseJailReleaseMethod(String playerId, JailMethod jailMethod) async {
-    String call = '$API_URL/choose_jail_release_method?player_id=$playerId';
+  Future<bool> getOutOfJail(String playerId, JailMethod jailMethod) async {
+    String call = '$API_URL/get_out_of_jail?player_id=$playerId';
     switch (jailMethod) {
       case JailMethod.doubles:
         call += '?method=doubles';
@@ -85,20 +115,29 @@ class EndpointService {
       case JailMethod.card:
         call += '?method=card';
     }
-    http.get(
+    final response = await http.get(
       Uri.parse(call),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'getOutOfJail');
+    return body['success'];
   }
 
-  void endTurn(String playerId) async {
-    server.get(
+  Future<bool> endTurn(String playerId) async {
+    final response = await http.get(
       Uri.parse('$API_URL/end_turn?player_id=$playerId'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'endTurn');
+    return body['success'];
   }
 
-  void reset({required PlayerId playerId}) async {
-    server.get(
-      Uri.parse('$API_URL/reset?player_id=${playerId.value}'),
+  Future<bool> reset({required PlayerId playerId}) async {
+    final response = await http.get(
+      Uri.parse('$API_URL/reset?player_id=$playerId'),
     );
+    final Json body = jsonDecode(response.body);
+    assert(body['event'] == 'reset');
+    return body['success'];
   }
 }
