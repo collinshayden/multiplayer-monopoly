74f22f44fca4bff6ce283e3c968e85c40508025c
agkhall <contact@agkhall.info>
2023-11-18 20:48:24 -0500

    Inserted new admin buttons

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:client/constants.dart';
 import 'package:client/cubit/game_cubit.dart';
 import 'package:client/model/player.dart';
+import 'package:client/view/admin_buttons.dart';
 import 'package:client/view/base/board.dart';
 import 'package:client/view/dice.dart';
 import 'package:client/view/player_info.dart';
@@ -53,7 +54,7 @@ class GameScreen extends StatelessWidget {
     return const Stack(
       children: [
         Board(),
-        AdminButtons(),
+        AdminPanel(),
         DisplayDice(),
       ],
     );
@@ -71,111 +72,6 @@ class PlayerDisplay extends StatelessWidget {
                 .players
                 .values
                 .toList());
-      },
-    );
-  }
-}
-
-class AdminButtons extends StatelessWidget {
-  const AdminButtons({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<GameCubit, GameState>(
-      builder: (context, state) {
-        return Align(
-          alignment: Alignment.topRight,
-          child: ExpandableTooltray(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<GameCubit>(context)
-                        .endpointService
-                        .reset(playerId: PlayerId("admin"));
-                  },
-                  child: const Text('Reset')),
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<GameCubit>(context)
-                        .registerPlayer(displayName: "testUser");
-                  },
-                  child: const Text('Join')),
-              ElevatedButton(
-                  onPressed: () {
-                    // BlocProvider.of<GameCubit>(context).updateGameData();
-
-                    BlocProvider.of<GameCubit>(context)
-                        .endpointService
-                        .startGame(playerId: PlayerId("admin"));
-                  },
-                  child: const Text('Start Game')),
-              ElevatedButton(
-                  onPressed: () {
-                    // BlocProvider.of<GameCubit>(context).updateGameData();
-                    PlayerId activePlayerId =
-                        BlocProvider.of<GameCubit>(context)
-                            .game
-                            .activePlayerId!;
-                    BlocProvider.of<GameCubit>(context)
-                        .rollDice(playerId: activePlayerId);
-                  },
-                  child: const Text('Roll')),
-              ElevatedButton(
-                  onPressed: () {
-                    // BlocProvider.of<GameCubit>(context).updateGameData();
-                    PlayerId activePlayerId =
-                        BlocProvider.of<GameCubit>(context)
-                            .game
-                            .activePlayerId!;
-                    BlocProvider.of<GameCubit>(context)
-                        .endTurn(playerId: activePlayerId);
-                  },
-                  child: const Text('End Turn')),
-              ElevatedButton(
-                  onPressed: () async {
-                    BlocProvider.of<GameCubit>(context).updateGameData();
-                  },
-                  child: const Text('Update State')),
-              ElevatedButton(
-                  onPressed: () async {
-                    BlocProvider.of<GameCubit>(context).switchActivePlayerId();
-                  },
-                  child: const Text('Change to Active Player')),
-              // Hardcoded as 0 for now. How can I get the tile ID the current player is on?
-
-              ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<GameCubit>(context).buyProperty(0);
-                  },
-                  child: const Text('Buy Property')),
-              MultiOptionWidget(
-                  defaultText: "Select Mortgage",
-                  options: const [
-                    {'text': 'Mortgage', 'value': true},
-                    {'text': 'Unmortgage', 'value': false},
-                  ],
-                  onPressed: (value) {
-                    BlocProvider.of<GameCubit>(context).setMortgage(0, value);
-                  }),
-              MultiOptionWidget(
-                  defaultText: "Select Jail method",
-                  options: const [
-                    {'text': 'Card', 'value': JailMethod.card},
-                    {'text': 'Doubles', 'value': JailMethod.doubles},
-                    {'text': 'Money', 'value': JailMethod.money},
-                  ],
-                  onPressed: (value) {
-                    BlocProvider.of<GameCubit>(context).getOutOfJail(value);
-                  }),
-              NumberInputWidget(
-                  buttonText: "Improvements",
-                  onPressed: (value) {
-                    BlocProvider.of<GameCubit>(context)
-                        .setImprovements(0, value);
-                  }),
-            ],
-          ),
-        );
       },
     );
   }
