4349d1b80190201b0ed67f0e1776d86389e7259b
agkhall <contact@agkhall.info>
2023-11-19 17:01:50 -0500

    Added more buttons

--- client/lib/view/admin_panel.dart
+++ client/lib/view/admin_panel.dart
@@ -27,6 +27,42 @@ class _AdminPanelState extends State<AdminPanel> {
         BlocProvider.of<GameCubit>(context).loadLocalConfig();
       },
     ),
+    AdminButton(
+      title: 'Update',
+      onPressed: (context) {
+        BlocProvider.of<GameCubit>(context).updateGameData(useAdmin: true);
+      },
+    ),
+    AdminTextInput(
+      title: 'Register Player',
+      onPressed: (context, text) {
+        BlocProvider.of<GameCubit>(context).registerPlayer(displayName: text);
+      },
+    ),
+    AdminButton(
+      title: 'Start Game',
+      onPressed: (context) {
+        BlocProvider.of<GameCubit>(context).startGame();
+      },
+    ),
+    AdminButton(
+      title: 'Roll Dice',
+      onPressed: (context) {
+        BlocProvider.of<GameCubit>(context).rollDice();
+      },
+    ),
+    AdminButton(
+      title: 'End Turn',
+      onPressed: (context) {
+        BlocProvider.of<GameCubit>(context).endTurn();
+      },
+    ),
+    AdminButton(
+      title: 'Reset Game',
+      onPressed: (context) {
+        BlocProvider.of<GameCubit>(context).resetGame();
+      },
+    ),
   ];
 
   ExpansionPanelList _buildPanel() {
@@ -62,7 +98,7 @@ class _AdminPanelState extends State<AdminPanel> {
     return SingleChildScrollView(
       child: Container(
         alignment: Alignment.topRight,
-        width: 300,
+        width: 500,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: _buildPanel(),
@@ -78,6 +114,12 @@ class _AdminPanelState extends State<AdminPanel> {
 /// else normally accessed through a `.of(context)` call.
 typedef ContextualCallback = void Function(BuildContext context);
 
+typedef ContextualTextInputCallback = void Function(
+    BuildContext context, String text);
+
+typedef ContextualNumberInputCallback = void Function(
+    BuildContext context, int number);
+
 /// A button used in the [AdminPanel] widget.
 ///
 /// This is one of a number of possible tools which can be included in the list
@@ -91,14 +133,118 @@ class AdminButton extends StatelessWidget {
   });
 
   final String title;
-  final ContextualCallback onPressed;
+  final onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
-      // Passes `context` into given callback, allowing it to find the GameCubit.
-      onTap: () => onPressed(context),
+        title: Text(title),
+        // Passes `context` into given callback, allowing it to find the GameCubit.
+        onTap: () => onPressed(context),
+        dense: true);
+  }
+}
+
+class AdminTextInput extends StatefulWidget {
+  const AdminTextInput({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final ContextualTextInputCallback onPressed;
+
+  @override
+  State<StatefulWidget> createState() => _AdminTextInputState();
+}
+
+class _AdminTextInputState extends State<AdminTextInput> {
+  TextEditingController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+      trailing: Row(
+        children: [
+          Expanded(
+            flex: 5,
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: IconButton(
+              splashRadius: 9,
+              color: Colors.blue[600],
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                widget.onPressed(context, _controller?.text ?? '');
+                _controller?.text = '';
+              },
+            ),
+          ),
+        ],
+      ),
+      dense: true,
+    );
+  }
+}
+
+class AdminTileValueShifter extends StatefulWidget {
+  const AdminTileValueShifter({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final ContextualNumberInputCallback onPressed;
+
+  @override
+  State<AdminTileValueShifter> createState() => _AdminTileValueShifterState();
+}
+
+class _AdminTileValueShifterState extends State<AdminTileValueShifter> {
+  var tileIds = List.generate(40, (index) => index);
+  int selectedTileId = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      // title: Text(widget.title),
+      trailing: SizedBox(
+        width: 120,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: DropdownButton(
+                  items: tileIds.map((e) {
+                    return DropdownMenuItem(value: e, child: Text('$e'));
+                  }).toList(),
+                  onChanged: (value) {
+                    selectedTileId = value ?? 0;
+                  }),
+            ),
+          ],
+        ),
+      ),
+      dense: true,
     );
   }
 }
