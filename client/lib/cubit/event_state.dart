3967b2a3a937fd7043d46534fee7393b1ffeeaa5
agkhall <contact@agkhall.info>
2023-11-20 14:15:28 -0500

    Created list of all events from EVENTS.md & enum

--- client/lib/cubit/event_state.dart
+++ client/lib/cubit/event_state.dart
@@ -1,1 +1,53 @@
 part of 'event_cubit.dart';
+
+// ROOT EVENT STATE
+
+sealed class EventState {}
+
+// BASE EVENT STATES
+
+class NullEvent extends EventState {}
+
+class ShowEvent extends EventState {}
+
+class PromptEvent extends EventState {}
+
+// CONCRETE EVENT STATES
+
+class ShowPlayerJoin extends ShowEvent {}
+
+class PromptStartGame extends PromptEvent {}
+
+class ShowStartGame extends ShowEvent {}
+
+class ShowStartTurn extends ShowEvent {}
+
+class ShowRoll extends ShowEvent {}
+
+class ShowPassGo extends ShowEvent {}
+
+class ShowRent extends ShowEvent {}
+
+class ShowTax extends ShowEvent {}
+
+class ShowMovePlayer extends ShowEvent {}
+
+class PromptPurchase extends PromptEvent {}
+
+class ShowPurchase extends ShowEvent {}
+
+class ShowImprovement extends ShowEvent {}
+
+class ShowMortgage extends ShowEvent {}
+
+class PromptEndTurn extends PromptEvent {}
+
+class ShowEndTurn extends ShowEvent {}
+
+class PromptLiquidate extends PromptEvent {}
+
+class ShowLoser extends ShowEvent {}
+
+class ShowEndGame extends ShowEvent {}
+
+class ShowBankruptcy extends ShowEvent {}
