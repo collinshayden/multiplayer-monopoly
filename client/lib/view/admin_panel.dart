ef89c4f6f2a279bf43fcc1f9672880fdfd142495
agkhall <contact@agkhall.info>
2023-11-20 13:14:54 -0500

    Incomplete implementation of admin buttons

--- client/lib/view/admin_panel.dart
+++ client/lib/view/admin_panel.dart
@@ -1,6 +1,6 @@
 import 'package:client/constants.dart';
 import 'package:client/cubit/game_cubit.dart';
-import 'package:client/view/widgets.dart';
+import 'package:client/model/player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,8 +11,10 @@ class AdminPanel extends StatefulWidget {
   State<AdminPanel> createState() => _AdminPanelState();
 }
 
+// DEFINE BUTTONS HERE!
 class _AdminPanelState extends State<AdminPanel> {
-  /// Whether the admin panel is currently open or not.
+  /// Whether the admin panel is currently open or not. By default, this widget
+  /// will load unexpanded.
   bool _isExpanded = false;
 
   /// A list of widgets used to construct the admin panel.
@@ -20,44 +22,68 @@ class _AdminPanelState extends State<AdminPanel> {
   List<Widget> items = [
     AdminButton(
       title: 'Load Local Config',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).loadLocalConfig();
+      onPressed: (gameCubit) {
+        gameCubit.loadLocalConfig();
       },
     ),
     AdminButton(
       title: 'Update',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).updateGameData(useAdmin: true);
+      onPressed: (gameCubit) {
+        gameCubit.updateGameData(useAdmin: true);
       },
     ),
     AdminTextInput(
       title: 'Register Player',
-      onPressed: (context, text) {
-        BlocProvider.of<GameCubit>(context).registerPlayer(displayName: text);
+      onPressed: (gameCubit, text) {
+        gameCubit.registerPlayer(displayName: text);
       },
     ),
+    const AdminClientSelector(title: 'Select Client'),
     AdminButton(
       title: 'Start Game',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).startGame();
+      onPressed: (gameCubit) {
+        gameCubit.startGame();
       },
     ),
     AdminButton(
       title: 'Roll Dice',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).rollDice();
+      onPressed: (gameCubit) {
+        gameCubit.rollDice();
       },
     ),
+    AdminTileButton(
+      title: 'Buy Property',
+      onPressed: (gameCubit, tileId) {
+        gameCubit.buyProperty(tileId);
+      },
+    ),
+    AdminImprovementsShifter(
+      title: 'Set Improvements',
+      onPressed: (gameCubit, tileId, number) {
+        gameCubit.setImprovements(tileId, number);
+      },
+    ),
+    AdminMortgageSetter(
+      title: 'Set Mortgage',
+      onPressed: (gameCubit, tileId, isMortgaged) {
+        gameCubit.setMortgage(tileId, isMortgaged);
+      },
+    ),
+    AdminJailButton(
+        title: 'Jail Method',
+        onPressed: (gameCubit, jailMethod) {
+          gameCubit.getOutOfJail(jailMethod);
+        }),
     AdminButton(
       title: 'End Turn',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).endTurn();
+      onPressed: (gameCubit) {
+        gameCubit.endTurn();
       },
     ),
     AdminButton(
       title: 'Reset Game',
-      onPressed: (context) {
-        BlocProvider.of<GameCubit>(context).resetGame();
+      onPressed: (gameCubit) {
+        gameCubit.resetGame(useAdmin: true);
       },
     ),
   ];
@@ -105,17 +131,23 @@ class _AdminPanelState extends State<AdminPanel> {
   }
 }
 
-/// Signature for callbacks used to create [AdminButtons]s. Callbacks which are
-/// assigned through this callback will be called with a [BuildContext] object
-/// and can thus access any inherited Bloc/Cubit objects, as well as anything
-/// else normally accessed through a `.of(context)` call.
-typedef ContextualCallback = void Function(BuildContext context);
+/// Signature for callbacks used to create [AdminButton]s. Callbacks which are
+/// assigned through this type will be called with a [GameCubit] object such
+/// that methods may be easily called on it.
+typedef CubitCallback = void Function(GameCubit cubit);
 
-typedef ContextualTextInputCallback = void Function(
-    BuildContext context, String text);
+/// Signature for callbacks used to create [AdminTextInput]s. Callbacks which
+/// are assigned through this type will be cakked with a [GameCubit] object and
+/// text representing the input currently in the [AdminTextInput].
+typedef TextInputCubitCallback = void Function(GameCubit cubit, String text);
 
-typedef ContextualNumberInputCallback = void Function(
-    BuildContext context, int number);
+typedef TileValueSetterCubitCallback<T> = void Function(
+    GameCubit cubit, int tileId, T value);
+
+typedef TileCubitCallback = void Function(GameCubit cubit, int tileId);
+
+typedef JailCubitCallback = void Function(
+    GameCubit cubit, JailMethod jailMethod);
 
 /// A button used in the [AdminPanel] widget.
 ///
@@ -130,14 +162,17 @@ class AdminButton extends StatelessWidget {
   });
 
   final String title;
-  final onPressed;
+  final CubitCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
         title: Text(title),
-        // Passes `context` into given callback, allowing it to find the GameCubit.
-        onTap: () => onPressed(context),
+        // Find and pass the Cubit to the callback.
+        onTap: () {
+          final gameCubit = BlocProvider.of<GameCubit>(context);
+          onPressed(gameCubit);
+        },
         dense: true);
   }
 }
@@ -150,7 +185,7 @@ class AdminTextInput extends StatefulWidget {
   });
 
   final String title;
-  final ContextualTextInputCallback onPressed;
+  final TextInputCubitCallback onPressed;
 
   @override
   State<StatefulWidget> createState() => _AdminTextInputState();
@@ -167,6 +202,7 @@ class _AdminTextInputState extends State<AdminTextInput> {
 
   @override
   void dispose() {
+    _controller?.dispose();
     super.dispose();
   }
 
@@ -174,69 +210,336 @@ class _AdminTextInputState extends State<AdminTextInput> {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(widget.title),
-      trailing: Row(
-        children: [
-          Expanded(
-            flex: 5,
-            child: TextField(
-              controller: _controller,
-              decoration: const InputDecoration(),
+      trailing: SizedBox(
+        width: 150,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(),
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: IconButton(
-              splashRadius: 9,
-              color: Colors.blue[600],
-              icon: const Icon(Icons.send),
-              onPressed: () {
-                widget.onPressed(context, _controller?.text ?? '');
-                _controller?.text = '';
-              },
+            Expanded(
+              flex: 1,
+              child: IconButton(
+                splashRadius: 20,
+                color: Colors.blue[600],
+                icon: const Icon(Icons.send),
+                onPressed: () {
+                  final gameCubit = BlocProvider.of<GameCubit>(context);
+                  widget.onPressed(gameCubit, _controller?.text ?? '');
+                  _controller?.text = '';
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       dense: true,
     );
   }
 }
 
-class AdminTileValueShifter extends StatefulWidget {
-  const AdminTileValueShifter({
+class AdminClientSelector extends StatefulWidget {
+  const AdminClientSelector({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<AdminClientSelector> createState() => _AdminClientSelectorState();
+}
+
+class _AdminClientSelectorState extends State<AdminClientSelector> {
+  PlayerId? selectedClientId;
+
+  @override
+  Widget build(BuildContext context) {
+    var playerList = BlocProvider.of<GameCubit>(context).game.players;
+    return BlocBuilder<GameCubit, GameState>(
+        builder: (innerContext, gameState) {
+      return ListTile(
+        title: Text(widget.title),
+        trailing: SizedBox(
+          width: 150,
+          child: Row(
+            children: [
+              DropdownButton(
+                isDense: true,
+                hint: Text(playerList[selectedClientId]?.displayName ?? 'None'),
+                items: playerList.keys
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text('${playerList[e]?.displayName}'),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedClientId = value;
+                  });
+                },
+              ),
+              IconButton(
+                splashRadius: 20,
+                color: Colors.blue[600],
+                icon: const Icon(Icons.send),
+                onPressed: () {
+                  final gameCubit = BlocProvider.of<GameCubit>(context);
+                  if (selectedClientId != null) {
+                    gameCubit.switchActivePlayerId(selectedClientId!);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+        dense: true,
+      );
+    });
+  }
+}
+
+class AdminTileButton extends StatefulWidget {
+  const AdminTileButton({
     super.key,
     required this.title,
     required this.onPressed,
   });
 
   final String title;
-  final ContextualNumberInputCallback onPressed;
+  final TileCubitCallback onPressed;
 
   @override
-  State<AdminTileValueShifter> createState() => _AdminTileValueShifterState();
+  State<AdminTileButton> createState() => _AdminTileButtonState();
 }
 
-class _AdminTileValueShifterState extends State<AdminTileValueShifter> {
+class _AdminTileButtonState extends State<AdminTileButton> {
   var tileIds = List.generate(40, (index) => index);
   int selectedTileId = 0;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      // title: Text(widget.title),
+      title: Text(widget.title),
       trailing: SizedBox(
-        width: 120,
+        width: 150,
         child: Row(
           children: [
-            Expanded(
-              flex: 2,
-              child: DropdownButton(
-                  items: tileIds.map((e) {
-                    return DropdownMenuItem(value: e, child: Text('$e'));
-                  }).toList(),
-                  onChanged: (value) {
-                    selectedTileId = value ?? 0;
-                  }),
+            DropdownButton(
+              isDense: true,
+              hint: Text('Tile: $selectedTileId'),
+              items: tileIds
+                  .map((e) => DropdownMenuItem(value: e, child: Text('$e')))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedTileId = value ?? 0;
+                });
+              },
+            ),
+            IconButton(
+              splashRadius: 20,
+              color: Colors.blue[600],
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                final gameCubit = BlocProvider.of<GameCubit>(context);
+                widget.onPressed(gameCubit, selectedTileId);
+              },
+            ),
+          ],
+        ),
+      ),
+      dense: true,
+    );
+  }
+}
+
+class AdminImprovementsShifter extends StatefulWidget {
+  const AdminImprovementsShifter({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final TileValueSetterCubitCallback<int> onPressed;
+
+  @override
+  State<AdminImprovementsShifter> createState() =>
+      _AdminImprovementsShifterState();
+}
+
+class _AdminImprovementsShifterState extends State<AdminImprovementsShifter> {
+  var tileIds = List.generate(40, (index) => index);
+  var improvementValues = List.generate(6, (index) => index);
+  int selectedTileId = 0;
+  int tileValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+      trailing: SizedBox(
+        width: 150,
+        child: Row(
+          children: [
+            DropdownButton(
+              isDense: true,
+              hint: Text('Tile: $selectedTileId'),
+              items: tileIds
+                  .map((e) => DropdownMenuItem(value: e, child: Text('$e')))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedTileId = value ?? 0;
+                });
+              },
+            ),
+            DropdownButton(
+              isDense: true,
+              hint: Text('$tileValue'),
+              items: improvementValues
+                  .map((e) => DropdownMenuItem(value: e, child: Text('$e')))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  tileValue = value ?? tileValue;
+                });
+              },
+            ),
+            IconButton(
+              splashRadius: 20,
+              color: Colors.blue[600],
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                final gameCubit = BlocProvider.of<GameCubit>(context);
+                widget.onPressed(gameCubit, selectedTileId, tileValue);
+              },
+            ),
+          ],
+        ),
+      ),
+      dense: true,
+    );
+  }
+}
+
+class AdminMortgageSetter extends StatefulWidget {
+  const AdminMortgageSetter({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final TileValueSetterCubitCallback<bool> onPressed;
+
+  @override
+  State<AdminMortgageSetter> createState() => _AdminMortgageSetterState();
+}
+
+class _AdminMortgageSetterState extends State<AdminMortgageSetter> {
+  List<int> tileIds = [];
+  int selectedTileId = 0;
+  bool selectedMortgage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    tileIds = BlocProvider.of<GameCubit>(context).getMortgageableTileIds();
+    return ListTile(
+      title: Text(widget.title),
+      trailing: SizedBox(
+        width: 150,
+        child: Row(
+          children: [
+            DropdownButton(
+              isDense: true,
+              hint: Text('Tile: $selectedTileId'),
+              items: tileIds
+                  .map((e) => DropdownMenuItem(value: e, child: Text('$e')))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedTileId = value ?? 0;
+                });
+              },
+            ),
+            DropdownButton(
+              alignment: AlignmentDirectional.centerEnd,
+              isDense: true,
+              items: [false, true]
+                  .map((e) => DropdownMenuItem(value: e, child: Text('$e')))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedMortgage = value ?? selectedMortgage;
+                });
+              },
+            ),
+            IconButton(
+              splashRadius: 20,
+              color: Colors.blue[600],
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                final gameCubit = BlocProvider.of<GameCubit>(context);
+                widget.onPressed(gameCubit, selectedTileId, selectedMortgage);
+              },
+            ),
+          ],
+        ),
+      ),
+      dense: true,
+    );
+  }
+}
+
+class AdminJailButton extends StatefulWidget {
+  const AdminJailButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  final JailCubitCallback onPressed;
+
+  @override
+  State<AdminJailButton> createState() => _AdminJailButtonState();
+}
+
+class _AdminJailButtonState extends State<AdminJailButton> {
+  JailMethod? selectedMethod;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+      trailing: SizedBox(
+        width: 150,
+        child: Row(
+          children: [
+            DropdownButton(
+              isDense: true,
+              hint: Text('Tile: ${selectedMethod ?? 'None'}'),
+              items: JailMethod.values
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedMethod = value;
+                });
+              },
+            ),
+            IconButton(
+              splashRadius: 20,
+              color: Colors.blue[600],
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                if (selectedMethod != null) {
+                  final gameCubit = BlocProvider.of<GameCubit>(context);
+                  widget.onPressed(gameCubit, selectedMethod!);
+                }
+              },
             ),
           ],
         ),
