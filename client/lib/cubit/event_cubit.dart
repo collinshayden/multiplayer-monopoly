7ec46ab0dfc20303df626c0e8e05611233969455
agkhall <contact@agkhall.info>
2023-11-20 14:14:50 -0500

    Base implementation + minor fix

--- client/lib/cubit/event_cubit.dart
+++ client/lib/cubit/event_cubit.dart
@@ -1,3 +1,9 @@
+import 'package:client/model/events.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 part 'event_state.dart';
 
-import 'package:client/model/events.dart';
+class EventCubit extends Cubit<EventState> {
+  EventCubit() : super(NullEvent());
+}
