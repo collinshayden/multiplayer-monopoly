c0f4904597e4b890736e1b316a161a40dda6582b
collinshayden <collinshayden171@gmail.com>
2023-11-22 22:03:13 -0500

    addded event states and made positioning relative

--- client/lib/view/game_screen/activity_feed.dart
+++ client/lib/view/game_screen/activity_feed.dart
@@ -34,21 +34,28 @@ class _ActivityFeedState extends State<ActivityFeed> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<EventCubit, EventState>(
-      buildWhen: (previous, current) {
-        return current is ShowRoll ||
-            current is ShowPlayerJoin ||
-            // TODO Add other event checks as needed
-            current is ShowBankruptcy;
-      },
-      builder: (context, state) {
-        _processState(state);
+    return BlocBuilder<EventCubit, EventState>(buildWhen: (previous, current) {
+      return current is ShowRoll ||
+          current is ShowPlayerJoin ||
+          current is ShowStartGame ||
+          current is ShowStartTurn ||
+          current is ShowPassGo ||
+          current is ShowRent ||
+          current is ShowPurchase ||
+          current is ShowImprovement ||
+          current is ShowEndTurn ||
+          current is ShowEndGame ||
+          current is ShowBankruptcy;
+    }, builder: (context, state) {
+      _processState(state);
+      return LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
         return Padding(
-          padding: const EdgeInsets.only(top: 300),
+          padding: EdgeInsets.only(top: constraints.maxHeight / 2.5),
           child: DisplayActivityFeed(activityList: _activityList),
         );
-      },
-    );
+      });
+    });
   }
 
   void _processState(EventState state) {
@@ -60,6 +67,43 @@ class _ActivityFeedState extends State<ActivityFeed> {
     } else if (state is ShowPlayerJoin) {
       final displayName = state.event.parameters["displayName"];
       _addItem("$displayName has joined the game!");
+    } else if (state is ShowStartGame) {
+      _addItem("The game has started.");
+    } else if (state is ShowStartTurn) {
+      final displayName = state.event.parameters["displayName"];
+      // _addItem("$displayName's turn has started.");
+    } else if (state is ShowEndTurn) {
+      final displayName = state.event.parameters["displayName"];
+      // _addItem("$displayName's turn has ended.");
+    } else if (state is ShowPassGo) {
+      final displayName = state.event.parameters["displayName"];
+      _addItem("$displayName has passed Go.");
+    } else if (state is ShowRent) {
+      final activePlayerName = state.event.parameters["activePlayerName"];
+      final landlordName = state.event.parameters["landlordName"];
+      final amount = state.event.parameters["amount"];
+      _addItem(
+          "$activePlayerName has landed on $landlordName's property and pays $amount");
+    } else if (state is ShowPurchase) {
+      final displayName = state.event.parameters["displayName"];
+      final propertyName = state.event.parameters["propertyName"];
+      _addItem("$displayName purchased $propertyName.");
+    } else if (state is ShowImprovement) {
+      final displayName = state.event.parameters["displayName"];
+      final propertyName = state.event.parameters["propertyName"];
+      final changeInImprovements =
+          state.event.parameters["changeInImprovements"];
+      if (changeInImprovements == -1) {
+        _addItem("$displayName sold an improvement on $propertyName.");
+      } else {
+        _addItem("$displayName bought an improvement on $propertyName.");
+      }
+    } else if (state is ShowBankruptcy) {
+      final displayName = state.event.parameters["displayName"];
+      _addItem("$displayName has gone bankrupt.");
+    } else if (state is ShowEndGame) {
+      final winnerName = state.event.parameters["winnerName"];
+      _addItem("The game has ended. $winnerName wins!");
     }
     // Handle other states similarly if needed
   }
